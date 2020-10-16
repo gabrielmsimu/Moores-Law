3c243d6ca20992f8284d0e8809e32ff153497663
gabrielmsimu <72418738+gabrielmsimu@users.noreply.github.com>
2020-10-16 17:12:34 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -5,25 +5,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace Moore_s_Law
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double x, y; 
+            double x, y;
             x = Math.Log(100, 2) * 1.5 * 12;
             y = Math.Log(100, 2) * 1.5;
-        
+
             Console.WriteLine("numarul de luni ca puterea de calcul sa se mareasca de 100 de ori la acelasi pret este");
-            Console.WriteLine(Math.Round(x, 3)); 
+            Console.WriteLine(Math.Round(x, 3));
             Console.WriteLine("in ani: ");
             Console.WriteLine(Math.Round(y, 3));
-          
-            Console.ReadKey();
 
+            Console.WriteLine("In continuare vom calcula numarul de luni/ani pentru ca puterea de calcul sa se maresca de un numar arbitrar de ori.");
+            Console.WriteLine("Introduceti un numar n");
+            double n = double.Parse(Console.ReadLine());
 
+            x = Math.Log(n, 2) * 1.5 * 12;
+            y = Math.Log(n, 2) * 1.5;
 
+            Console.WriteLine("numarul de luni ca puterea de calcul sa se mareasca de {0} de ori la acelasi pret este", n);
+            Console.WriteLine(Math.Round(x, 3));
+            Console.WriteLine("in ani: ");
+            Console.WriteLine(Math.Round(y, 3));
         }
     }
 }
